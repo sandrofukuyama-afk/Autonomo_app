32ef11ddb28a4396ae47b45eb61837069e61d610
sandrobrjp <sandrofukuyama@gmail.com>
2026-03-26 08:47:07 +0900

    fix: change AuthFlowType from pkce to implicit to bypass cross-browser session issues

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ Future<void> main() async {
           anonKey:
               'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6YXp3cGdqbmNvd2t1ZGtkaGNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MDIyODAsImV4cCI6MjA4ODM3ODI4MH0.mQBxjBlgPQpxb5-QyFNhgitM_WOnWlkEzFStYZPr5Pk',
           authOptions: const FlutterAuthClientOptions(
-            authFlowType: AuthFlowType.pkce,
+            authFlowType: AuthFlowType.implicit,
           ),
         );
 
